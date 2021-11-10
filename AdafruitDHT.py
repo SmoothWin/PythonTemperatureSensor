ecc7351859c99cd593d6d823a87985c44ac368a0
BunTymofiy <tima.boon@gmail.com>
2021-11-10 01:06:48 -0500

    Added db_insert and db_get_all - Working but need refinement

--- AdafruitDHT.py
+++ AdafruitDHT.py
@@ -1,41 +1,47 @@
 #!/usr/bin/python
 # Copyright (c) 2014 Adafruit Industrie
+import sys
+
 from board import *
-from datetime import datetime
+# from datetime import datetime
 import adafruit_dht
-import schedule
-import time
-import mysql.connector
+# import schedule
+# import time
+from pylint.lint import Run
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="admin",
-  password="Nature38:built:"
-)
-
-print(mydb)
+import DbHandler
 
 SENSOR_PIN = D17
 dht11 = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
-now = datetime.now()
+try:
+    temperature_c = dht11.temperature
+except RuntimeError:
+    print("sheesh")
+    sys.exit(1)
 
+insert_result = DbHandler.db_insert("temperature","temperature",temperature_c)
+print(insert_result, "record inserted.")
 
-def job():
-    try:
-        temperature_c = dht11.temperature
-        time.sleep(1)
-        humidity = dht11.humidity
-        time.sleep(1)
+myresult = DbHandler.db_get_all("temperature")
+for x in myresult:
+    print(x)
 
-        print(f"Humidity = {humidity:.2f}%")
-        print(f"Temperature = {temperature_c:.2f}°C")
-
-    except RuntimeError:
-        print("sucks to be you")
-
-
-schedule.every(10).seconds.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# def job():
+#     try:
+#         temperature_c = dht11.temperature
+#         time.sleep(1)
+#         humidity = dht11.humidity
+#         time.sleep(1)
+#
+#         print(f"Humidity = {humidity:.2f}%")
+#         print(f"Temperature = {temperature_c:.2f}°C")
+#
+#     except RuntimeError:
+#         print("sucks to be you")
+#
+#
+# schedule.every(10).seconds.do(job)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
--- DbHandler.py
+++ DbHandler.py
@@ -2,22 +2,29 @@ import mysql.connector
 from datetime import datetime
 
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="admin",
-    password="Nature38:built:"
-)
-
-
 def db_insert(table_name, field_name, field_value):
+    mydb = mysql.connector.connect(host="localhost", user="admin", password="Nature38:built:", database="dht_db")
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print("date and time =", dt_string)
     mycursor = mydb.cursor()
-    sql = f"INSERT INTO {table_name} ({field_name}) VALUES (%s, %s)"
-    val = (field_value)
+    sql = f"INSERT INTO {table_name} ({field_name},time) VALUES (%s, %s)"
+    val = (field_value, now)
     mycursor.execute(sql, val)
-
     mydb.commit()
+    mydb.close()
+    return mycursor.rowcount
 
-    print(mycursor.rowcount, "record inserted.")
+
+def db_get_all(table_name):
+
+    mydb = mysql.connector.connect(host="localhost", user="admin", password="Nature38:built:", database="dht_db")
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute(f"SELECT * FROM {table_name}")
+
+    myresult = mycursor.fetchall()
+    mydb.close()
+    return myresult
+
+
+
